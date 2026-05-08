571972552f9120db94a1b5422d89dd8691693e75
renanjfa <renanjusan@gmail.com>
2026-05-08 18:31:01 -0300

    Component Organization in MinhasViagens

--- src/components/MinhasViagens.js
+++ src/components/MinhasViagens.js
@@ -1,5 +1,7 @@
 import { Component } from 'react';
-import { Image, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+import TripCard from './TripCard';
 
 class MinhasViagens extends Component {
 
@@ -12,91 +14,32 @@ class MinhasViagens extends Component {
                 <Text style={styles.title}>Minhas Viagens</Text>
             
             
-                    {/* BOTÃO */}
-                    <TouchableOpacity style={styles.newTripButton}>
-                        <Text style={styles.newTripText}>+ Nova Viagem</Text>
-                    </TouchableOpacity>
+                {/* BOTÃO */}
+                <TouchableOpacity style={styles.newTripButton}>
+                    <Text style={styles.newTripText}>+ Nova Viagem</Text>
+                </TouchableOpacity>
 
 
+                {/* LISTA DE VIAGENS */}
+                <ScrollView>
 
-                    {/* LISTA DE VIAGENS */}
-                    <ScrollView>
+                    <View style={styles.cardsContainer}>
 
-                        <View style={styles.cardsContainer}>
+                        <TripCard/>
+                        <TripCard/>
+                        <TripCard/>
+                        <TripCard/>
+                        <TripCard/>
+                        <TripCard/>
+                        <TripCard/>
+                        <TripCard/>
+                        <TripCard/>
+                        <TripCard/>
+                        
 
-                        {/* CARD */}
-                        <TouchableOpacity style={styles.tripCard}>
+                    </View>
 
-                            <Image
-                            source={require('../assets/arrasca.webp')}
-                            style={styles.tripImage}
-                            />
-
-                            <View style={styles.tripInfo}>
-
-                            <Text style={styles.tripTitle}>
-                                Viagem 50 Anos Itália
-                            </Text>
-
-                            <Text style={styles.tripDescription}>
-                                Viagem de comemoração...
-                            </Text>
-
-                            </View>
-
-                        </TouchableOpacity>
-
-
-
-                        {/* CARD */}
-                        <TouchableOpacity style={styles.tripCard}>
-
-                            <Image
-                            source={require('../assets/arrasca.webp')}
-                            style={styles.tripImage}
-                            />
-
-                            <View style={styles.tripInfo}>
-
-                            <Text style={styles.tripTitle}>
-                                Férias no Rio
-                            </Text>
-
-                            <Text style={styles.tripDescription}>
-                                Viagem muito gostosa...
-                            </Text>
-
-                            </View>
-
-                        </TouchableOpacity>
-
-
-
-                        {/* CARD */}
-                        <TouchableOpacity style={styles.tripCard}>
-
-                            <Image
-                            source={require('../assets/arrasca.webp')}
-                            style={styles.tripImage}
-                            />
-
-                            <View style={styles.tripInfo}>
-
-                            <Text style={styles.tripTitle}>
-                                Viagem a Paris
-                            </Text>
-
-                            <Text style={styles.tripDescription}>
-                                Viagem pela europa...
-                            </Text>
-
-                            </View>
-
-                        </TouchableOpacity>
-
-                        </View>
-
-                    </ScrollView>
+                </ScrollView>
 
             </View>
 
@@ -159,59 +102,5 @@ const styles = StyleSheet.create({
 
 
 
-    /* CARD DA VIAGEM */
-    tripCard: {
-        width: 270,
-
-        backgroundColor: '#FFFFFF',
-
-        borderRadius: 12,
-
-        overflow: 'hidden',
-
-        marginBottom: 20,
-
-        shadowColor: '#000',
-        shadowOffset: {
-        width: 0,
-        height: 2,
-        },
-        shadowOpacity: 0.08,
-        shadowRadius: 4,
-
-        elevation: 3,
-    },
-
-
-
-    /* IMAGEM */
-    tripImage: {
-        width: '100%',
-        height: 180,
-
-        resizeMode: 'cover',
-    },
-
-
-
-    /* INFORMAÇÕES */
-    tripInfo: {
-        padding: 18,
-    },
-
-    tripTitle: {
-        fontSize: 22,
-        fontWeight: 'bold',
-
-        color: '#222222',
-
-        marginBottom: 12,
-    },
-
-    tripDescription: {
-        fontSize: 16,
-        lineHeight: 24,
-
-        color: '#777777',
-    },
+    
 });
